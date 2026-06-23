80afbec9d893ff1b514ab0e591c4809e489dba13
Harsh Bhosale <harshpbhosale215@gmail.com>
2026-06-23 23:47:20 +0530

    Initial commit: TaskForge project management app with Inngest and Prisma

---
+++ server/inngest/index.js
@@ -0,0 +1,57 @@
+import { Inngest } from "ingest";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "taskforge" });
+
+//inngest function to sync user creation from clerk to prisma
+const syncUserCreation = inngest.createFunction(
+  {id: 'sync-user-from-clerk'},
+  {event: 'clerk/user.created'},
+  async ({ event })=>{
+    const {data} = event
+    await prisma.user.create({
+    data: {
+        id: data.id,
+        email: data?.email_addresses[0]?.email_addresses,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+    }
+    })
+  }
+)
+
+//inngest function to delete user from database when user is deleted from clerk
+const syncUserDeletion = inngest.createFunction(
+  {id: 'delete-user-from-clerk'},
+  {event: 'clerk/user.deleted'},
+  async ({ event })=>{
+    const {data} = event
+    await prisma.user.delete({
+    where: {
+        id: data.id,
+    }
+    })
+  }
+)
+
+//inngest function to sync user update from clerk to prisma
+const syncUserUpdation = inngest.createFunction(
+  {id: 'update-user-from-clerk'},
+  {event: 'clerk/user.updated'},
+  async ({ event })=>{
+    const {data} = event
+    await prisma.user.update({
+    where: {
+        id: data.id,
+    },
+    data: {
+        email: data?.email_addresses[0]?.email_addresses,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+    }
+    })
+  }
+)
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
